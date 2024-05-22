6a60beb45cd44f0032715e0e2d65f489776a6873
Mako <skb@gmail.com>
2024-05-22 21:09:58 +0300

    create db, inser new_word commit

---
+++ database.py
@@ -0,0 +1,49 @@
+import sqlite3
+import logging
+
+logging.basicConfig(filename='logs.txt', level=logging.INFO,
+                    format="%(asctime)s FILE: %(filename)s IN: %(funcName)s MESSAGE: %(message)s", filemode="w")
+
+
+def create_database():
+    try:
+        with sqlite3.connect('db.sqlite') as con:
+            cursor = con.cursor()
+
+            cursor.execute('''
+            CREATE TABLE IF NOT EXISTS dict (
+            id INTEGER PRIMARY KEY,
+            user_id INTEGER,
+            word TEXT,
+            trans TEXT,
+            date INTEGER,
+            level INTEGER DEFAULT 0,
+            error INTEGER DEFAULT 0
+            ''')
+            logging.info('DATABASE: База данных создана')
+    except Exception as e:
+        logging.error(e)
+        return None
+
+
+def new_word(user_id, full_message):
+    try:
+        with sqlite3.connect('db.sqlite') as con:
+            cur = con.cursor()
+
+            word, trans, date = full_message
+
+            cur.execute(
+                '''
+                INSERT INTO dict (user_id, word, trans, date)
+                VALUES (?, ?, ?, ?) 
+                ''',
+                (user_id, word, trans, date)
+            )
+            con.commit()
+            logging.info('DATABASE: INSERT INTO dict'
+                         f'VALUES ({user_id}, {word}, {trans}, {date})')
+
+    except Exception as e:
+        logging.error(e)
+        return None
